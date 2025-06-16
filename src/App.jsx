ed9b6d4e29cff2bb84ed1bfd449cf900584c6e66
carolineglass <carolineglass89@gmail.com>
2025-06-16 19:46:00 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { createRoot } from "react-dom/client";
+import Pizza from "./Pizza";
+
+const App = () => {
+    return (
+      <div>
+        <h1>Pizza Menu</h1>
+        <Pizza name="Pepperoni" description="Mozzarella Cheese, Pepperoni" />
+        <Pizza
+          name="The Hawaiian Pizza"
+          description="Sliced Ham, Pineapple, Mozzarella Cheese"
+        />
+        <Pizza
+          name="The Big Meat Pizza"
+          description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
+        />
+      </div>
+    );
+  };
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
