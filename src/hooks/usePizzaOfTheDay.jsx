817df63bff86923982e4a3e589e03df04f176dc1
Caroline <gypsychever@gmail.com>
2025-07-03 14:41:06 -0400

    fix fetch url in pizza of the day

--- src/hooks/usePizzaOfTheDay.jsx
+++ src/hooks/usePizzaOfTheDay.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 export const usePizzaOfTheDay = () => {
   const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
   const [loading, setLoading] = useState(true);
 
   const fetchPizzaOfTheDay = async () => {
     try {
-      const resp = await fetch("/api/pizza-of-the-day");
+      const resp = await fetch(`${apiUrl}/api/pizza-of-the-day`);
       const data = await resp.json();
       setPizzaOfTheDay(data);
     } catch (error) {
